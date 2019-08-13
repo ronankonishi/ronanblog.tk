f10ffc946ec139cf4cd0e83dfe8290f9808541b4
Ronan Konishi <rkonishi@ucsd.edu>
2019-08-13 09:15:12 -0700

    added about content

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.base')
 
 @section('content')
-	<div class="main">
+	<div class="main about">
+		<h3>Intro</h3>
+		<p>Hello there! My name is Ronan Konishi. I enjoy working with technology, exercising, and playing music. Read on to learn more about me!</p>
+
+		<h3>Education</h3>
+		<p>I am a second year undergrad studying Computer Science at the University of California San Diego. At the University of California San Diego, I work under a professor as a Software Developer at the Qualcomm Institute - UCSD Division of Calit2. I am also a member of IEEE at UCSD and have participated in the 2018 Winter Quarterly project.</p>
+
+		<h3>Personal Projects</h3>
+		<p>In addition to my university studies, I work on numerous side projects. Currently, I am developing a Smart Device that allows wireless control of devices in a room (fans, door lock, music station), a mobile smartphone game, and this blog site.</p>
+
+		<h3>Passions</h3>
+		<p>Aside from developer life, I enjoy rock climbing, surfing, and playing basketball. I go indoor rock climbing several times a week, and occasionally go on outdoor climbing excursions. I plan to incorporate climbing into my weekly schedule when I return to college. UCSD is also the perfect place to surf with the amazing waves and weather!</p>
+
 	</div>
 @endsection
